344d98e29d738471cd4cf75f651b009a17f52643
Emmett Rayes <emmett.rayes@icloud.com>
2024-12-07 13:36:11 +0100

    Add day 2 part 1 tests

--- src/main/scala/Day2.scala
+++ src/main/scala/Day2.scala
@@ -3,4 +3,12 @@ object Day2 {
 
     def parse(input: String): Input =
         input.split("\n").map(_.split("\\s+").toList.map(_.toInt)).toList
+
+    object Part1 {
+        private type Output = Int
+
+        def solve(input: Input): Output = {
+            ???
+        }
+    }
 }
--- src/test/scala/Day2Tests.scala
+++ src/test/scala/Day2Tests.scala
@@ -1,7 +1,8 @@
 class Day2Tests extends munit.FunSuite {
     test("parsing") {
         val input =
-            """|1 2 7 8 9
+            """|7 6 4 2 1
+               |1 2 7 8 9
                |9 7 6 2 1
                |1 3 2 4 5
                |8 6 4 4 1
@@ -9,6 +10,7 @@ class Day2Tests extends munit.FunSuite {
                |""".stripMargin
 
         val expected = List(
+          List(7, 6, 4, 2, 1),
           List(1, 2, 7, 8, 9),
           List(9, 7, 6, 2, 1),
           List(1, 3, 2, 4, 5),
@@ -16,6 +18,28 @@ class Day2Tests extends munit.FunSuite {
           List(1, 3, 6, 7, 9),
         )
         val obtained = Day2.parse(input)
+        assertEquals(clue(obtained), expected)
+    }
+
+    test("part1 example") {
+        val input =
+            """|7 6 4 2 1
+               |1 2 7 8 9
+               |9 7 6 2 1
+               |1 3 2 4 5
+               |8 6 4 4 1
+               |1 3 6 7 9
+               |""".stripMargin
+
+        val expected = 2
+        val obtained = Day2.Part1.solve(Day2.parse(input))
+        assertEquals(clue(obtained), expected)
+    }
+
+    test("part1 solution") {
+        val input = os.read(os.Path(getClass.getResource("Day2").toURI))
+        val expected = 0
+        val obtained = Day2.Part1.solve(Day2.parse(input))
         assertEquals(clue(obtained), expected)
     }
 }
