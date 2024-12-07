5faec6f04336ac38c02aa8ef8d9494171711e247
Emmett Rayes <emmett.rayes@icloud.com>
2024-12-07 12:39:39 +0100

    Implement day 1 part 1 parsing

--- src/main/scala/Day1.scala
+++ src/main/scala/Day1.scala
@@ -3,7 +3,15 @@ object Day1 {
         type Input = (List[Int], List[Int])
 
         def parse(input: String): Input = {
-            ???
+            val array = input
+                .split("\n")
+                .map(_.split("\\s+"))
+                .map(_.map(_.toInt))
+                .transpose
+                .map(_.toList)
+
+            array match
+                case Array(first, second) => (first, second)
         }
 
         def solve(input: Input): Int = {
