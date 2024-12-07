5dcc62c72b60ec72248e783ddb1f25dccb1cd34f
Emmett Rayes <emmett.rayes@icloud.com>
2024-12-07 12:11:20 +0100

    Add day 1 part 1 test

---
+++ src/main/scala/Day1.scala
@@ -0,0 +1,13 @@
+object Day1 {
+    object Part1 {
+        type Input = (List[Int], List[Int])
+
+        def parse(input: String): Input = {
+            ???
+        }
+
+        def solve(input: Input): Int = {
+            ???
+        }
+    }
+}
---
+++ src/test/scala/Day1Tests.scala
@@ -0,0 +1,41 @@
+class Day1Tests extends munit.FunSuite {
+    test("part1 parsing") {
+        val input =
+            """|3   4
+               |4   3
+               |2   5
+               |1   3
+               |3   9
+               |3   3
+               |""".stripMargin
+
+        val expected = (
+          List(3, 4, 2, 1, 3, 3),
+          List(4, 3, 5, 3, 9, 3),
+        )
+        val obtained = Day1.Part1.parse(input)
+        assertEquals(clue(obtained), expected)
+    }
+
+    test("part1 example") {
+        val input =
+            """|3   4
+               |4   3
+               |2   5
+               |1   3
+               |3   9
+               |3   3
+               |""".stripMargin
+
+        val expected = 11
+        val obtained = Day1.Part1.solve(Day1.Part1.parse(input))
+        assertEquals(clue(obtained), expected)
+    }
+
+    test("part1 solution") {
+        val input = os.read(os.Path(getClass.getResource("Day1/Part1").toURI))
+        val expected = 0
+        val obtained = Day1.Part1.solve(Day1.Part1.parse(input))
+        assertEquals(clue(obtained), expected)
+    }
+}
