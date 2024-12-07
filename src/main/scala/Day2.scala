954df3bb5b60d9b62e57e305b8419f46ec5a7442
Emmett Rayes <emmett.rayes@icloud.com>
2024-12-07 13:34:24 +0100

    Implement day 2 parsing

--- src/main/scala/Day2.scala
+++ src/main/scala/Day2.scala
@@ -1,7 +1,6 @@
 object Day2 {
     private type Input = List[List[Int]]
 
-    def parse(input: String): Input = {
-        ???
-    }
+    def parse(input: String): Input =
+        input.split("\n").map(_.split("\\s+").toList.map(_.toInt)).toList
 }
