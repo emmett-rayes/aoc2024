e750b748a10a0e287eb720217a61a06e5692d23c
Emmett Rayes <emmett.rayes@icloud.com>
2024-12-10 17:57:22 +0100

    Implement day 4 parsing

--- src/main/scala/Day4.scala
+++ src/main/scala/Day4.scala
@@ -1,5 +1,7 @@
 object Day4 {
     private type Input = List[List[Char]]
 
-    def parse(input: String): Input = ???
+    def parse(input: String): Input =
+        input.split("\n").map(_.toCharArray.toList).toList
+
 }
