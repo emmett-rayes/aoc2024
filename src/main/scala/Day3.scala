a24b30402e0d014ee4d5403dff79e27c112ab3bc
Emmett Rayes <emmett.rayes@icloud.com>
2024-12-10 16:42:18 +0100

    Add day 3 part 1 tests

--- src/main/scala/Day3.scala
+++ src/main/scala/Day3.scala
@@ -2,4 +2,10 @@ object Day3 {
     private type Input = String
 
     def parse(input: String): Input = input
+
+    object Part1 {
+        private type Output = Int
+
+        def solve(input: Input): Output = ???
+    }
 }
--- src/test/scala/Day3Tests.scala
+++ src/test/scala/Day3Tests.scala
@@ -5,4 +5,18 @@ class Day3Tests extends munit.FunSuite {
         val obtained = Day3.parse(input)
         assertEquals(clue(obtained), expected)
     }
+
+    test("part1 example") {
+        val input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+        val expected = 161
+        val obtained = Day3.Part1.solve(Day3.parse(input))
+        assertEquals(clue(obtained), expected)
+    }
+
+    test("part1 solution") {
+        val input = os.read(os.Path(getClass.getResource("Day3").toURI))
+        val expected = 0
+        val obtained = Day3.Part1.solve(Day3.parse(input))
+        assertEquals(clue(obtained), expected)
+    }
 }
