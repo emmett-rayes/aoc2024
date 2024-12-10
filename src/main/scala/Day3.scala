09af07aaff88eed1c4aab705a613bbe6975c5240
Emmett Rayes <emmett.rayes@icloud.com>
2024-12-10 16:40:45 +0100

    Add day 3 parsing test

---
+++ src/main/scala/Day3.scala
@@ -0,0 +1,5 @@
+object Day3 {
+    private type Input = String
+
+    def parse(input: String): Input = ???
+}
---
+++ src/test/scala/Day3Tests.scala
@@ -0,0 +1,8 @@
+class Day3Tests extends munit.FunSuite {
+    test("parsing") {
+        val input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+        val expected = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+        val obtained = Day3.parse(input)
+        assertEquals(clue(obtained), expected)
+    }
+}
