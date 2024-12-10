da4e45bb1a1613f3b5b6aa04cb9aaa555c33165d
Emmett Rayes <emmett.rayes@icloud.com>
2024-12-10 17:16:49 +0100

    Refactor day 3 part 1 solution

--- src/main/scala/Day3.scala
+++ src/main/scala/Day3.scala
@@ -6,30 +6,37 @@ object Day3 {
     object Part1 {
         private type Output = Int
 
+        private enum State {
+            case MatchingChar(char: Char)
+            case MatchingX(digits: String = "")
+            case MatchingY(x: Int, digits: String = "")
+        }
+
         def solve(input: Input): Output = {
             import scala.collection.mutable
 
             val results = mutable.Buffer.empty[(Int, Int)]
-            var state: (Char, String, String) = ('m', "", "")
+            val initial = State.MatchingChar('m')
+            var state = initial
             for c <- input do {
                 state = state match
-                    case (char @ ('m' | 'u' | 'l' | '('), "", "") if c == char =>
+                    case State.MatchingChar(char @ ('m' | 'u' | 'l' | '(')) if c == char =>
                         char match
-                            case 'm' => ('u', "", "")
-                            case 'u' => ('l', "", "")
-                            case 'l' => ('(', "", "")
-                            case '(' => ('x', "", "")
-                    case ('x', left, right) if c.isDigit =>
-                        ('x', left + c, right)
-                    case ('x', left, right) if c == ',' =>
-                        ('y', left, right)
-                    case ('y', left, right) if c.isDigit =>
-                        ('y', left, right + c)
-                    case ('y', left, right) if c == ')' =>
-                        results.append((left.toInt, right.toInt))
-                        ('m', "", "")
+                            case 'm' => State.MatchingChar('u')
+                            case 'u' => State.MatchingChar('l')
+                            case 'l' => State.MatchingChar('(')
+                            case '(' => State.MatchingX()
+                    case State.MatchingX(digits) if c.isDigit =>
+                        State.MatchingX(digits + c)
+                    case State.MatchingX(digits) if c == ',' =>
+                        State.MatchingY(digits.toInt)
+                    case State.MatchingY(x, digits) if c.isDigit =>
+                        State.MatchingY(x, digits + c)
+                    case State.MatchingY(x, digits) if c == ')' =>
+                        results.append((x, digits.toInt))
+                        initial
                     case _ =>
-                        ('m', "", "")
+                        initial
             }
             results.map(r => r._1 * r._2).sum
         }
